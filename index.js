fd16df36c1e8a991d4e450c519c90337c6a56ae5
Lifter <Sosite.my_dick@gmail.com>
2025-06-16 19:30:24 -0400

    Add errorMiddleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const errorMid = require("./middleware/error-midleware.js")
 const router = require("./router/index.js");
 
 const app = express()
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use("/",router);
+app.use(errorMid)
 
 const start = async () => {
   try{
