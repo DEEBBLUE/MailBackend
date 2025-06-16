fb79d0b48d3f52375d24d844ca07c2dc2f8e1977
Lifter <Sosite.my_dick@gmail.com>
2025-06-16 08:59:23 -0400

    Add fuzzi controller in router

--- router/index.js
+++ router/index.js
@@ -1,10 +1,12 @@
 const Router = require("express").Router
 const usetController = require("../controlers/user-controller.js")
+const fuzzingController = require("../controlers/fuzzy-controller.js")
 
 const router = new Router()
 
 router.post("/auth",usetController.auth)
 router.post("/logOut",usetController.logOut)
 router.get("/refresh",usetController.refresh)
+router.get("/fuzzing",fuzzingController.fuzzing)
 
 module.exports = router
