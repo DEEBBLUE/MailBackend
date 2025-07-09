de81561d73d7995ce9ce73fed1ad669cfe9f2b4d
Lifter <Sosite.my_dick@gmail.com>
2025-07-09 18:37:48 -0400

    Create model for message

--- DatabaseService/pkg/Models/Models.go
+++ DatabaseService/pkg/Models/Models.go
@@ -11,14 +11,13 @@ type User struct{
 }
 
 func CreateUser(email,password,name string) *User{
-	return &User{
-		Email: email,
+	return &User{ Email: email,
 		Password: password,
 		Name: name,
 	}
 }
 
-func ToGRPC(user *User) *types.User {
+func ToGRPCUser(user *User) *types.User {
 	return &types.User{
 		Email: user.Email,
 		Password: user.Password,
@@ -26,7 +25,7 @@ func ToGRPC(user *User) *types.User {
 	}
 }
 
-func ToModel(grpc *types.User) *User {
+func ToModelUser(grpc *types.User) *User {
 	return CreateUser(
 		grpc.GetEmail(),
 		grpc.GetName(),
@@ -34,4 +33,38 @@ func ToModel(grpc *types.User) *User {
 	)
 }
 
+type Message struct{
+	Id int
+	Name string 
+	Publisher_email string
+	Consumers_emails []string
+	Message string
+	TimeDispatch string
+}
+func CreateMessage(id int,name string,message string,time_dispatch string,
+									 publisher_email string,consumers_emails []string,) *Message{
+	return &Message{
+		Id: id,
+		Name: name,
+		Publisher_email: publisher_email,
+		Consumers_emails: consumers_emails,
+		Message: message,
+		TimeDispatch: time_dispatch,
+	}
+}
 
+func ToGRPCMessage(message *Message) *types.Message{
+	return &types.Message{
+		Id: int32(message.Id),
+		Name: message.Name,
+		PublisherEmail: message.Publisher_email,
+		EmailsConsumers: message.Consumers_emails,
+		MessageValue: message.Message,
+		TimeDispatch: message.TimeDispatch,
+	}
+}
+
+func ToModelMessage(message *types.Message) *Message{
+	return CreateMessage(int(message.GetId()),message.GetName(),message.MessageValue,message.GetTimeDispatch(),
+											 message.GetPublisherEmail(),message.EmailsConsumers)	
+}
