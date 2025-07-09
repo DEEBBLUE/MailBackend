0294fd80ea812ad38a7b33341f47e192f01b1a93
Lifter <Sosite.my_dick@gmail.com>
2025-07-09 15:26:02 -0400

    Create main file in which start a server

--- DatabaseService/cmd/main.go
+++ DatabaseService/cmd/main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"database/config"
+	handlers "database/internal/Handlers"
+	orm "database/internal/Orm"
+	"database/sql"
+	"net"
+
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
 )
 
-func main() {
-	fmt.Println("Hello")
+func main() {	
+	conf,err := config.LoadConfig("/home/lifter/mail/backend/DatabaseService/config/config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	db,err := sql.Open("postgres",conf.GetStringConf())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	ormDB := orm.Init(db)
+
+	listener,err := net.Listen("tcp",":4444")
+	if err != nil {
+		panic(err)
+	}
 	
+	server := grpc.NewServer()
+	handlers.InitDatabaseService(server,ormDB)
+
+	if err := server.Serve(listener); err != nil{
+		panic(err)
+	}
 }
