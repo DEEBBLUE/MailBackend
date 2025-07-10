4d43c752fbb6d0ef88abc971a07430be63c87449
Lifter <Sosite.my_dick@gmail.com>
2025-07-10 18:53:17 -0400

    Init auth service

--- AuthService/cmd/main.go
+++ AuthService/cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	handlers "authService/internal/Handlers"
+	"net"
+
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	serv := grpc.NewServer()
+	handlers.InitAuthServer(serv)
+	
+	listner,err := net.Listen("tcp",":4443")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := serv.Serve(listner); err != nil {
+		panic(err)
+	}
 }
+
