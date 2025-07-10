2c28cd4c0fe18d5b58b1a0a51dae5af942eb4e6a
Lifter <Sosite.my_dick@gmail.com>
2025-07-10 12:59:15 -0400

    Create model for worw with JWT

---
+++ AuthService/pkg/Models/JWT.go
@@ -0,0 +1,32 @@
+package jwtAuth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func CreateAccess(payload string,cred string) (string,error){
+	claims := jwt.MapClaims{
+		"email": payload,
+		"exp": time.Now().Add(time.Minute * 15).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+	return token.SignedString(cred)
+
+}
+
+func ValidateToken(token string,cred string) (*jwt.Token,error){
+	return jwt.Parse(token,func(t *jwt.Token) (interface{},error) {
+		return cred,nil
+	}) 
+}
+
+func CreateRefresh(data string,cred string) (string,error) {
+	claims := jwt.MapClaims{
+		"hash_password": data,
+		"exp": time.Now().Add(time.Hour * 360).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+	return token.SignedString(cred)
+}
