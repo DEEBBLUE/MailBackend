9d6c5ae4adfb9cbfd7c680784b59bc5a1a40f379
Lifter <Sosite.my_dick@gmail.com>
2025-07-10 18:52:05 -0400

    Change type cred field

--- AuthService/pkg/Models/JWT.go
+++ AuthService/pkg/Models/JWT.go
@@ -2,7 +2,6 @@ package jwtAuth
 
 import (
 	"time"
-
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -12,13 +11,13 @@ func CreateAccess(payload string,cred string) (string,error){
 		"exp": time.Now().Add(time.Minute * 15).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
-	return token.SignedString(cred)
+	return token.SignedString([]byte(cred))
 
 }
 
 func ValidateToken(token string,cred string) (*jwt.Token,error){
 	return jwt.Parse(token,func(t *jwt.Token) (interface{},error) {
-		return cred,nil
+		return []byte(cred),nil
 	}) 
 }
 
@@ -28,5 +27,5 @@ func CreateRefresh(data string,cred string) (string,error) {
 		"exp": time.Now().Add(time.Hour * 360).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
-	return token.SignedString(cred)
+	return token.SignedString([]byte(cred))
 }
