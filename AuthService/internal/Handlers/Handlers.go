e680cf375a69ebc46240bfcc63b70a798ab688d4
Lifter <Sosite.my_dick@gmail.com>
2025-07-10 21:48:17 -0400

    Impl auth service

--- AuthService/internal/Handlers/Handlers.go
+++ AuthService/internal/Handlers/Handlers.go
@@ -9,6 +9,7 @@ import (
 
 	authService "github.com/DEEBBLUE/MailProtos/api/Auth"
 	dbService "github.com/DEEBBLUE/MailProtos/api/Database"
+	"github.com/DEEBBLUE/MailProtos/api/Req"
 	_ "github.com/DEEBBLUE/MailProtos/api/Types"
 
 	. "github.com/DEEBBLUE/MailProtos/api/Req"
@@ -54,9 +55,8 @@ func(auth *AuthServer) Register(
 	creatersErr := make(chan error,1)	
 
 	userEmail := req.GetUser().GetEmail()
-	userName := req.GetUser().GetName()
 	userPass := req.GetUser().GetPassword()
-	
+
 	go func(errChan chan error){
 		_,err := auth.dbClient.CreateUser(ctx,req)
 		if err != nil {
@@ -66,7 +66,7 @@ func(auth *AuthServer) Register(
 	}(creatersErr)
 
 
-	payload := fmt.Sprintf("Email: %s,Name: %s",userEmail,userName)
+	payload := fmt.Sprintf("Email: %s",userEmail)
 	access_token,err := jwtAuth.CreateAccess(payload,auth.conf.Credential) 
 	if err != nil {
 		return nil,fmt.Errorf("Error when generate access_token,%w",err)
@@ -89,5 +89,70 @@ func(auth *AuthServer) Register(
 	return &TokensRes{
 		AccessToken: access_token,
 		RefreshToken: refresh_token,
+	},nil
+}
+
+func(auth *AuthServer) Login(
+	ctx context.Context,
+	req *authService.LoginReq,
+)(*Req.TokensRes,error) {
+
+	user,err := auth.dbClient.RepeateUser(context.Background(),&RepeateUserReq{
+		Email: req.GetEmail(),
+	})
+	if err != nil {
+		return nil,err
+	}
+	if req.GetPassword() == user.GetUser().GetPassword(){
+		payload := fmt.Sprintf("Email: %s",req.GetEmail())
+		access_token,err := jwtAuth.CreateAccess(payload,auth.conf.Credential) 
+		if err != nil {
+			return nil,fmt.Errorf("Error when generate access_token,%w",err)
+		}
+
+		refresh_token,err := jwtAuth.CreateRefresh(req.GetPassword(),auth.conf.Credential)
+		if err != nil {
+			return nil,fmt.Errorf("Error when generate refresh_token,%w",err)
+		}
+
+		return &TokensRes{
+			AccessToken: access_token,
+			RefreshToken: refresh_token,
+		},nil
+	}
+	return nil,fmt.Errorf("User data is incorrect")
+}
+
+func(auth *AuthServer) Access(
+	ctx context.Context,
+	req *authService.AccessReq,
+)( *Req.DefaultRes,error) {
+	token,err := jwtAuth.ValidateToken(req.GetAccessToken(),auth.conf.Credential)		
+
+	if err != nil || !token.Valid{
+		return nil,fmt.Errorf("Access token err")
+	}
+
+	return &DefaultRes{
+		Status: "Ok",
+	},nil
+}
+func(auth *AuthServer) Refresh(
+	ctx context.Context,
+	req *authService.RefreshReq,
+) ( *authService.RefreshRes,error){
+
+	token,err := jwtAuth.ValidateToken(req.GetRefreshToken(),auth.conf.Credential)	
+	if err != nil || !token.Valid{
+		return nil,fmt.Errorf("Refresh token err")
+	}
+
+	access_token,err := jwtAuth.CreateAccess(req.GetEmail(),auth.conf.Credential)	
+	if err != nil {
+		return nil,fmt.Errorf("Access token err")
+	}
+
+	return &authService.RefreshRes{
+		AccessToken: access_token,
 	},nil
 }
